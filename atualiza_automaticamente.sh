df218acb341caf66fe7a5eb8e6eaca44709604e8
André Caregnato <andrecaregnato@yahoo.com.br>
2018-01-10 09:15:33 -0200

    Add files via upload

--- atualiza_automaticamente.sh
+++ atualiza_automaticamente.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-reiniciar=reboot
-desligar=halt
-if (!= $desligar or $reiniciar)
-apt-get update; apt-get upgrade -y
+sudo apt-get update; sudo apt update; apt list --upgradable; sudo aptitude update; sudo apt-get upgrade -y; sudo apt upgrade -y; sudo aptitude upgrade -y;
+sudo apt-get clean; sudo apt-get autoclean; sudo apt-get autoremove -y; sudo dpkg --configure -a; 
+
+
 
 
